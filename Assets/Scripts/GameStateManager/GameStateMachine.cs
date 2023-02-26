8f78275921969598e01e9ed44b15b6d13a0381d9
Michael Poole <michaelpoole10@gmail.com>
2023-02-26 01:45:47 +0000

    Implimented Missing States

--- Assets/Scripts/GameStateManager/GameStateMachine.cs
+++ Assets/Scripts/GameStateManager/GameStateMachine.cs
@@ -6,13 +6,18 @@ namespace GameStateManager
 {
     public class GameStateMachine : MonoBehaviour
     {
+        // References
         public InputManager _input { get; private set; }
 
+        // States
         public readonly GamePlayState PlayState = new();
         public readonly GamePauseState PauseState = new();
+        public readonly GameDeathState DeathState = new();
+        public readonly GameLoadingState LoadingState = new();
+        public readonly GameMainMenuState MainMenuState = new();
+        
+        // Components
         public Rigidbody Rb;
-        
-        
         private BaseGameState _currentState;
 
         public void Awake()
--- Assets/Scripts/GameStateManager/States/GameMainMenuState.cs
+++ Assets/Scripts/GameStateManager/States/GameMainMenuState.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameMainMenuState : MonoBehaviour
+namespace GameStateManager.States
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameMainMenuState : BaseGameState
     {
-        
-    }
+        public override void OnStart()
+        {
+            throw new System.NotImplementedException();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnLeave()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
