f774317529249a5aae898e0b9493936759b66d39
Michael Poole <michaelpoole10@gmail.com>
2023-03-16 21:20:38 +0000

    obstacle manager base stuff finished

--- Assets/Scripts/ObstacleManager/BaseObs.cs
+++ Assets/Scripts/ObstacleManager/BaseObs.cs
@@ -8,6 +8,8 @@ namespace ObstacleManager
         private readonly int _damageValue = 5;
         private void OnTriggerEnter(Collider other)
         {
+            if (other.gameObject.GetComponent<IDamageable>() is null) return;
+            
             other.gameObject.GetComponent<IDamageable>().DamageHealth(_damageValue);
             if (other.gameObject.GetComponent<IDamageable>() != null) 
             {
@@ -15,6 +17,10 @@ namespace ObstacleManager
                 Debug.Log("Destroyed");
             }
         }
-        
+
+        private void Update()
+        {
+            transform.position += Vector3.back * Time.deltaTime * 100;
+        }
     }
 }
